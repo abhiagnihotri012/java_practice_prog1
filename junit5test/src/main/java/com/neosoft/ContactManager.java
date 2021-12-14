4cde5fb8a6d3b0548df4e2cc4fbaf47e6ebb60ec
abhiagnihotri012 <abhihackera@gmail.com>
2021-12-14 09:16:19 +0530

    commit 5: junit5 practice work

---
+++ junit5test/src/main/java/com/neosoft/ContactManager.java
@@ -0,0 +1,38 @@
+package com.neosoft;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+//Mobile phone contact manager
+public class ContactManager {
+
+	Map<String, Contact> contactList = new ConcurrentHashMap<String, Contact>();
+	
+	public void addContact(String firstName,String lastName, String phoneNumber) {
+		Contact contact = new Contact(firstName, lastName, phoneNumber);
+		validateContact(contact);
+		checkIfContactAlreadyExist(contact);
+		contactList.put(generateKey(contact), contact);
+	}
+
+	public Collection<Contact> getAllContacts(){
+		return contactList.values();
+	}
+	
+	private void validateContact(Contact contact) {
+		contact.validateFirstName();
+		contact.validateLastName();
+		contact.validatePhoneNumber();
+	}
+
+	private void checkIfContactAlreadyExist(Contact contact) {
+		if(contactList.containsKey(generateKey(contact)))
+			throw new RuntimeException("Contact Already Exists!!");
+	}
+	
+	private String generateKey(Contact contact) {		
+		return String.format("%s-%s", contact.getFirstName(),contact.getLastName());
+	}	
+	
+}
